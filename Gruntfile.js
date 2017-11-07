021357d2fa947ba833dc949d85f183519aaa2d7f
Thomas Massmann <thomas.massmann@it-spir.it>
2017-11-07 12:05:46 -0500

    Clean up theme package.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,101 +1,117 @@
 module.exports = function (grunt) {
-    'use strict';
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        // we could just concatenate everything, really
-        // but we like to have it the complex way.
-        // also, in this way we do not have to worry
-        // about putting files in the correct order
-        // (the dependency tree is walked by r.js)
-        less: {
-            dist: {
-                options: {
-                    paths: [],
-                    strictMath: false,
-                    sourceMap: true,
-                    outputSourceFiles: true,
-                    sourceMapFileInline: true,
-                    sourceMapURL: '++theme++barceloneta-ng/less/theme-compiled.less.map',
-                    sourceMapFilename: 'less/theme-compiled.less.map',
-                    modifyVars: {
-                        "isPlone": "false"
-                    }
-                },
-                files: {
-                    'less/theme-compiled.css': 'less/theme.local.less',
-                }
-            }
+  'use strict';
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // we could just concatenate everything, really
+    // but we like to have it the complex way.
+    // also, in this way we do not have to worry
+    // about putting files in the correct order
+    // (the dependency tree is walked by r.js)
+    less: {
+      dev: {
+        options: {
+          compress: false,
+          paths: [],
+          strictMath: false,
+          sourceMap: true,
+          sourceMapFileInline: true
         },
-        postcss: {
-            options: {
-                map: true,
-                processors: [
-                    require('autoprefixer')({
-                        browsers: ['last 2 versions']
-                    })
-                ]
-            },
-            dist: {
-                src: 'less/*.css'
-            }
-        },
-        watch: {
-            scripts: {
-                files: [
-                    'less/*.less',
-                    'barceloneta/less/*.less'
-                ],
-                tasks: ['less', 'postcss']
-            }
-        },
-        browserSync: {
-            html: {
-                bsFiles: {
-                    src : [
-                      'less/*.less',
-                      'barceloneta/less/*.less'
-                    ]
-                },
-                options: {
-                    watchTask: true,
-                    debugInfo: true,
-                    online: true,
-                    server: {
-                        baseDir: "."
-                    },
-                }
-            },
-            plone: {
-                bsFiles: {
-                    src : [
-                      'less/*.less',
-                      'barceloneta/less/*.less'
-                    ]
-                },
-                options: {
-                    watchTask: true,
-                    debugInfo: true,
-                    proxy: "localhost:8080",
-                    reloadDelay: 3000,
-                    // reloadDebounce: 2000,
-                    online: true
-                }
-            }
+        files: {
+          'less/theme.css': 'less/theme.less',
         }
-    });
+      },
+      dist: {
+        options: {
+          compress: true,
+          paths: [],
+          strictMath: false,
+          sourceMap: false,
+        },
+        files: {
+          'less/theme.css': 'less/theme.less',
+        }
+      }
+    },
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require('autoprefixer')({
+            browsers: ['last 2 versions']
+          })
+        ]
+      },
+      dev: {
+        options: {
+          map: true,
+        },
+        src: 'less/*.css'
+      },
+      dist: {
+        map: false,
+        src: 'less/*.css'
+      }
+    },
+    watch: {
+      less: {
+        files: [
+          'less/*.less',
+          'less/**/*.less'
+        ],
+        tasks: ['less:dev', 'postcss:dev']
+      }
+    },
+    browserSync: {
+      html: {
+        bsFiles: {
+          src : [
+            'less/*.css',
+            '*.html'
+          ]
+        },
+        options: {
+          watchTask: true,
+          debugInfo: true,
+          online: true,
+          server: {
+            baseDir: "."
+          },
+        }
+      },
+      plone: {
+        bsFiles: {
+          src : [
+            'less/*.css',
+            'rules/*.xml',
+            '*.html',
+            '*.xml'
+          ]
+        },
+        options: {
+          watchTask: true,
+          debugInfo: true,
+          proxy: "localhost:8080",
+          reloadDelay: 3000,
+          // reloadDebounce: 2000,
+          online: true
+        }
+      }
+    }
+  });
 
 
-    // grunt.loadTasks('tasks');
-    grunt.loadNpmTasks('grunt-browser-sync');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-postcss');
+  // grunt.loadTasks('tasks');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
 
-    // CWD to theme folder
-    grunt.file.setBase('./src/plonetheme/barcelonetang/theme');
+  // CWD to theme folder
+  grunt.file.setBase('./src/plonetheme/barcelonetang/theme');
 
-    grunt.registerTask('compile', ['less', 'postcss']);
-    grunt.registerTask('default', ['compile']);
-    grunt.registerTask('bsync', ["browserSync:html", "watch"]);
-    grunt.registerTask('plone-bsync', ["browserSync:plone", "watch"]);
+  grunt.registerTask('release', ['less:dist', 'postcss:dist']);
+  grunt.registerTask('compile', ['less:dev', 'postcss:dev']);
+  grunt.registerTask('default', ['release']);
+  grunt.registerTask('bsync', ['browserSync:html', 'watch']);
+  grunt.registerTask('bsync-plone', ['browserSync:plone', 'watch']);
 };
--- setup.py
+++ setup.py
@@ -5,58 +5,70 @@ from setuptools import find_packages
 from setuptools import setup
 
 
+version = '0.1.dev0'
+description = 'The Barceloneta NG Theme for Plone 5.'
 long_description = '\n\n'.join([
     open('README.rst').read(),
     open('CONTRIBUTORS.rst').read(),
     open('CHANGES.rst').read(),
 ])
 
-
 install_requires = [
+    'setuptools',
+    # -*- Extra requirements: -*-
     'Products.GenericSetup>=1.8.2',
+    'collective.themefragments',
+    'collective.themesitesetup',
     'plone.api',
     'plone.app.theming',
     'plonetheme.barceloneta',
-    'setuptools',
-    'z3c.jbot',
+    'spirit.plone.theming',
 ],
+
+testfixture_requires = [
+    'plone.app.mosaic',
+    'plone.app.standardtiles',
+    'webcouturier.dropdownmenu',
+]
 
 
 setup(
     name='plonetheme.barcelonetang',
-    version='1.0.dev0',
-    description="The NG default theme for Plone 5",
+    version=version,
+    description=description,
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Framework :: Plone :: 5.1rc1",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 5.0',
+        'Framework :: Plone :: 5.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
     ],
-    keywords='Python Plone',
-    author='Thomas Massmann',
-    author_email='thomas.massmann@it-spir.it',
+    keywords='plone diazo',
+    author='it-spirit',
+    author_email='development@it-spir.it',
     url='https://github.com/it-spirit/plonetheme.barcelonetang',
     download_url='https://pypi.python.org/pypi/plonetheme.barcelonetang',
     license='GPL version 2',
     packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['plonetheme'],
     package_dir={'': 'src'},
+    namespace_packages=['plonetheme'],
     include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
-    extras_require={
-        'test': [
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
+    extras_require=dict(
+        testfixture=testfixture_requires,
+        test=[
+            'plone.app.robotframework',
             'plone.app.testing',
             'robotframework-selenium2screenshots',
         ],
-    },
+    ),
     entry_points="""
     [z3c.autoinclude.plugin]
     target = plone
